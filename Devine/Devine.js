25c45f23a347e07cf9cc14a2d5b2cb8cf857c854
Amandine <amandine.labaeye@orange.fr>
2018-10-31 16:58:41 +0100

    Commit

---
+++ Devine/Devine.js
@@ -0,0 +1,53 @@
+
+var NombreAleatoire = 0;
+
+var NombreEssaies = 10;
+
+function Replay () {
+
+    NombreAleatoire = (Math.random() * 100) + 1;
+
+    NombreAleatoire = Math.floor(NombreAleatoire);
+
+    NombreEssaies = 10;
+
+    document.getElementById('Bouton').style.display= "block";
+    document.getElementById('Nombre').style.display= "block";
+}
+
+document.getElementById('Bouton').addEventListener("click", function () {
+
+    var Valeur = document.getElementById("Nombre").value;
+
+    if (NombreAleatoire > Valeur) {
+        alert('Retente ta chance. Le nombre est supérieur à ta proposition');
+        Essaie ();
+    }
+
+    if (NombreAleatoire < Valeur) {
+        alert('Retente ta chance. Le nombre est inférieur à ta proposition');
+        Essaie ();
+    }
+
+    if (NombreAleatoire == Valeur) {
+        alert('Bravo tu as trouvé le nombre !');
+    }
+
+});
+
+document.getElementById('TEXT').innerHTML = "Attention !!" + "<br>" +"Il te reste" + " " + NombreEssaies + " " + "essais";
+
+function Essaie () {
+    NombreEssaies--;
+    document.getElementById('TEXT').innerHTML = "Attention !!" + "<br>" +"Il te reste" + " " + NombreEssaies + " " + "essais";
+
+    if (NombreEssaies <= 0) {
+       document.getElementById('Bouton').style.display= "none";
+       document.getElementById('Nombre').style.display= "none";
+       alert('Désolé tu as perdu. Tu peux cliquer sur le bouton "Reset" pour retenter ta chance !');
+    }
+}
+
+
+
+document.getElementById('Replay').addEventListener("click", Replay);
