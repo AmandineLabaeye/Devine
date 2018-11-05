c09c731f9fa7dac8c76999a9d70c74122233503e
Amandine <amandine.labaeye@orange.fr>
2018-11-05 15:03:27 +0100

    Commit 4

--- Devine/Devine.js
+++ Devine/Devine.js
@@ -2,41 +2,45 @@ var NombreAleatoire = 0;
 
 var NombreEssaies = 10;
 
-function Nombre () {
+
 
     NombreAleatoire = (Math.random() * 100) + 1;
 
     NombreAleatoire = Math.floor(NombreAleatoire);
-}
+console.log(NombreAleatoire);
 
 function Replay () {
+    NombreAleatoire = (Math.random() * 100) + 1;
 
-    Nombre();
-
+    NombreAleatoire = Math.floor(NombreAleatoire);
+    console.log(NombreAleatoire);
     NombreEssaies = 10;
+
+    document.getElementById('TEXT').innerHTML = "Attention !!" + "<br>" +"Il te reste" + " " + NombreEssaies + " " + "essais";
 
     document.getElementById('Bouton').style.display= "block";
     document.getElementById('Nombre').style.display= "block";
+    document.getElementById('Historique').innerHTML = "Historique : " + "<br>";
 }
 
 document.getElementById('Bouton').addEventListener("click", function () {
 
-    Nombre();
-
     var Valeur = document.getElementById("Nombre").value;
 
+    document.getElementById('Historique').innerHTML += Valeur + "<br>";
+
     if (NombreAleatoire > Valeur) {
-        alert('Retente ta chance. Le nombre est supérieur à ta proposition');
+        document.getElementById('Indication').innerHTML = 'Retente ta chance.' + '<br>' + 'Le nombre est supérieur à ta proposition';
         Essaie ();
     }
 
     if (NombreAleatoire < Valeur) {
-        alert('Retente ta chance. Le nombre est inférieur à ta proposition');
+        document.getElementById('Indication').innerHTML ='Retente ta chance.'  + '<br>' + 'Le nombre est inférieur à ta proposition';
         Essaie ();
     }
 
     if (NombreAleatoire == Valeur) {
-        alert('Bravo tu as trouvé le nombre !');
+        document.getElementById('Indication').innerHTML ='Bravo tu as trouvé le nombre !';
     }
 
 });
@@ -50,7 +54,7 @@ function Essaie () {
     if (NombreEssaies <= 0) {
        document.getElementById('Bouton').style.display= "none";
        document.getElementById('Nombre').style.display= "none";
-       alert('Désolé tu as perdu. Tu peux cliquer sur le bouton "Reset" pour retenter ta chance !');
+       alert('Désolé tu as perdu.' + " " + 'Le nombre était' + " " + NombreAleatoire + " " + 'Tu peux cliquer sur le bouton "Reset" pour retenter ta chance !');
     }
 }
 
